2bdfae332dd3b9901375f44f7f6dc7493a0beea2
Mist-co <47158592+Mist-co@users.noreply.github.com>
2020-01-12 13:33:34 +0100

    Update function.h

--- function.h
+++ function.h
@@ -82,21 +82,88 @@ void insertionsort (int data[],int n)
     }
 }
 
-int invint(int N)
+template <typename T>
+T invint(T N)
 {
-    int T = N, R = 0;
-    while (T > 0)
+    T temp = N, R = 0;
+    while (temp > 0)
     {
-        R = R * 10 + (T % 10);
-        T /= 10;
+        R = R * 10 + (temp % 10);
+        temp /= 10;
     }
 
     return R;
 }
 
-int mcd(int a, int b)
+template <typename T>
+vector<T>* divisorOf(T n)
 {
-  int resto = a % b;
+    vector<T>* divisor;
+    for (T i=1; i<=sqrt(n); ++i)
+        if (n%i == 0)
+        {
+            if (n/i == i)
+                divisor->push_back(i);
+            else
+            {
+                divisor->push_back(i);
+                divisor->push_back(n/i);
+            }
+        }
+    sort(divisor->begin(), divisor->end());
+    return divisor;
+}
+
+void print(vector<int> data, char type='T', char sep='\n')
+{
+    cout << "----- ELENCO -----\n";
+    for (auto i=data.begin(); i!=data.end(); i++)
+        cout << *i << sep;
+    cout << "==================\n";
+
+}
+
+template <typename T>
+void print(T data, char type='T', char sep='\n')
+{
+    cout << "----- Valore -----\n";
+        cout << data << sep;
+    cout << "==================\n";
+}
+
+template <typename T>
+T r_factorial(T n)
+{
+    return (n>1) ? n * r_factorial(n - 1) : 1;
+}
+
+template <typename T>
+T l_factorial(T n)
+{
+    T factorial = 1;
+    for(int i = 1; i <=n; ++i)
+        factorial *= i;
+    return factorial;
+}
+
+template <typename T>
+bool isPrime (T n)
+{
+    T a;
+
+    if (n == 1)
+        return false;
+
+    for(a = 2; a <= sqrt(n); a++)
+        if (( n % a )== 0)
+            return false;
+    return true;
+}
+
+template <typename T>
+T mcd(T a, T b)
+{
+  T resto = a % b;
   while(resto != 0)
   {
     a = b;
@@ -106,30 +173,29 @@ int mcd(int a, int b)
   return b;
 }
 
-int gcd(int v[], int N)
+template <typename T>
+T gcd(T v[], T N)
 {
-    int a = v[0];
-    for(int i=1; i<N; i++)
-    {
+    T a = v[0];
+    for(int i=1; i<N; i++
         a = mcd(a, v[i]);
-    }
     return a;
 }
 
-int mcm(int a, int b)
+template <typename T>
+T mcm(T a, T b)
 {
-    int temp = mcd(a, b);
+    T temp = mcd(a, b);
 
     return temp ? (a / temp * b) : 0;
 }
 
-int lcm(int v[], int N)
+template <typename T>
+T lcm(T v[], T N)
 {
-    int a = v[0];
+    T a = v[0];
     for(int i=1; i<N; i++)
-    {
         a = mcm(a, v[i]);
-    }
     return a;
 }
 
@@ -138,9 +204,7 @@ string invstr(string s)
     int l=s.length();
     string c;
     for (int i=l; i>0; i--)
-    {
         c = c + s.at(i-1);
-    }
     s=c;
 
     return s;
@@ -157,13 +221,9 @@ int dectobin(int n)
         r = n%2;
         n = q;
         if(r==0)
-        {
             bin = '0' + bin;
-        }
         else
-        {
             bin = '1' + bin;
-        }
     }
 
     n = atoi(bin.c_str());
